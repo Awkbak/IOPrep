53b78dcdc77f34ab923dd552376f196442881429
MuriloHome <murigun007@gmail.com>
2016-09-28 15:50:18 -0400

    Few changes to scripts made

---
+++ IOGame/Assets/Scripts/Button.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+    private bool signal = false;
+	// Use this for initialization
+	void Start () {
+        signal = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void PressButton()
+    {
+        signal = !signal;
+    }
+}
