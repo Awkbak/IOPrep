e07487738a7ec4b2e85ede8261840e0ece5d6bd5
Kirsten <camlin@carolina.rr.com>
2016-10-27 18:30:55 -0700

    menu screen start

---
+++ IOGame/Assets/Scripts/Movement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        obj.transform.Translate(new Vector3(.1f, 0, 0));
+	}
+}
