9000545f7927330da6c25ac38c27ee84bd95a0c4
Kirsten <camlin@carolina.rr.com>
2016-12-06 14:43:18 -0500

    re-tooling of game: bits instead of buttons and potential help character

---
+++ IOGame/Assets/Scripts/ByteSpin.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ByteSpin : MonoBehaviour {
+
+    public GameObject center;
+    public float speed;
+
+    public bool truth;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (truth)
+        {
+            transform.Rotate(Vector3.one, speed * Time.deltaTime);
+            transform.RotateAround(center.transform.position, Vector3.right, speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Rotate(Vector3.one, speed * Time.deltaTime);
+            transform.RotateAround(center.transform.position, Vector3.up, speed * Time.deltaTime);
+        }
+        
+	}
+
+}
